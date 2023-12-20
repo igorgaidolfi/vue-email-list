bd3c55424e2975180320b32b734a61f8344c783d
igorgaidolfi <gandalf.4@outlook.it>
2023-12-20 15:21:04 +0100

    update Es. completato

--- js/script.js
+++ js/script.js
@@ -2,11 +2,17 @@ const {createApp} = Vue
 createApp({
     data(){
         return{
-
+            mail:[]
         }
     
     },
-    methods(){
-
+    created(){
+        for(let i=0; i<10;i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) =>{
+                this.mail.push(response.data.response)
+        })   
+        }
+    },
+    methods: {
     }
 }).mount('#app')
